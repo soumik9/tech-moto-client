fb0fde78ecced2ec3e7012a15eda41d02480d6d6
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-22 07:01:03 +0600

    router, bootstrap, query and navbar setuped

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Home from './pages/Home/Home';
+import { Routes, Route } from "react-router-dom";
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="">
- hi
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import logo  from '../../../assets/logo.jpg'
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+        <Container>
+
+            <Navbar.Brand href="#home">
+                <img alt="" src={logo} width="30" height="30"  className="d-inline-block align-top" />
+                {' '}
+                Tech Moto
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link as={ Link } to='/'>Home</Nav.Link>
+                    <Nav.Link as={ Link } to='/'>About</Nav.Link>
+                    <Nav.Link as={ Link } to='/'>Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    );
+};
+
+export default Header;
