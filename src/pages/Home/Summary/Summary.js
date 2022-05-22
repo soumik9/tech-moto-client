1f81d92fe8b1f7051b497802aacd494b01ab8051
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-23 03:47:34 +0600

    bussineess summary with responsive completed

---
+++ src/pages/Home/Summary/Summary.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { FaUsers, FaMapMarkedAlt, FaTools } from 'react-icons/fa'
+import { MdOutlineRateReview } from 'react-icons/md'
+import './summary.css'
+
+const Summary = () => {
+
+    const summaries = [
+        {icon: <FaUsers className='summary__item-icon mb-3' />, count: 500, text: 'Happy Customers'},
+        {icon: <FaMapMarkedAlt className='summary__item-icon mb-3' />, count: 65, text: 'Countries'},
+        {icon: <FaTools className='summary__item-icon mb-3' />, count: 1500, text: 'Tools'},
+        {icon: <MdOutlineRateReview className='summary__item-icon mb-3' />, count: 800, text: 'Reviews'},
+    ]
+
+    return (
+        <section className='summary my-50'>
+            <Container>
+
+                <div className="summary__header">
+                    <Row className='justify-content-center'>
+                        <Col md={6}>
+                            <div className='text-center'>
+                                <h2 className='text-uppercase'>Trusted Bussiness to worldwide</h2>
+                                <p>Best bike parts provider in the world</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+
+                <div className="summary__body mt-5">
+                    <Row>
+                        {
+                            summaries.map(summary => (
+                                <Col md={3}>
+                                <div className="summary__item">
+                                    <Card>
+                                        <Card.Body className='text-center'>
+                                            {summary.icon} 
+                                            <h2>{summary.count}</h2>
+                                            <p>{summary.text}</p>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            </Col>
+                            ))
+                        }
+                    </Row>
+                </div>
+
+            </Container>
+        </section>
+    );
+};
+
+export default Summary;
